0df64ac293f0996655a2d71480d4cf37f456271b
Leonardo Sanner <leosanner12@gmail.com>
2026-03-06 17:31:53 -0300

    feat: plan entity

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -18,4 +18,11 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+      },
+    },
+  },
 });
