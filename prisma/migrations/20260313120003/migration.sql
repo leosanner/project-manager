75d9a4e7c3b91d4e8db7bfc3989296b9b0c55bac
Leonardo Sanner <leosanner12@gmail.com>
2026-03-13 09:01:36 -0300

    .

--- prisma/migrations/20260313120003/migration.sql
+++ prisma/migrations/20260313120003/migration.sql
@@ -4,5 +4,11 @@
   - A unique constraint covering the columns `[plantype]` on the table `Plan` will be added. If there are existing duplicate values, this will fail.
 
 */
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "planId" TEXT;
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Plan_plantype_key" ON "Plan"("plantype");
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_planId_fkey" FOREIGN KEY ("planId") REFERENCES "Plan"("id") ON DELETE SET NULL ON UPDATE CASCADE;
